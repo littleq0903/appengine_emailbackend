85130c7d8b4a5a1e48568f65479858199acc6e87
LittleQ <littleq0903@gmail.com>
2013-03-18 11:45:26 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name="AppEngine Email Backend",
+        version="1.0",
+        description="Django wrapper for App Engine email APIs.",
+        packages=['appengine_emailbackend'],
+        )
